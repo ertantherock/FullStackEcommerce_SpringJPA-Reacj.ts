9aa32977746a1094d3eb2e764a532c0dd553a7ab
Ertan Kaya <107481123+ertantherock@users.noreply.github.com>
2023-08-23 02:24:54 +0300

    Improvement Continues

---
+++ Spring-React-EcommerceApp/src/main/java/com/ertankaya/springreactecommerceapp/entities/projections/IProduct.java
@@ -0,0 +1,12 @@
+package com.ertankaya.springreactecommerceapp.entities.projections;
+
+public interface IProduct {
+    String getProductName();
+    String getDetail();
+    Integer getPrice();
+    String getCname();
+
+    Long getPid();
+
+    String getText();
+}
--- Spring-React-EcommerceApp/src/main/java/com/ertankaya/springreactecommerceapp/repositories/ProductRepository.java
+++ Spring-React-EcommerceApp/src/main/java/com/ertankaya/springreactecommerceapp/repositories/ProductRepository.java
@@ -1,7 +1,21 @@
 package com.ertankaya.springreactecommerceapp.repositories;
 
 import com.ertankaya.springreactecommerceapp.entities.Product;
+import com.ertankaya.springreactecommerceapp.entities.projections.IProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByNameEquals(String productName);
+
+
+    @Query(value = "select p.productName,p.detail,p.price, c.cname from category c inner join product p on c.cid = p.cid where c.cname = ?1 order by p.price asc limit ?2", nativeQuery = true)
+    List<IProduct> getProductsBy(String cname, int limit);
+
+    @Query(value = "select p.productName,p.detail,p.price, c.categoryName,pi.text,pi.pid from category c inner join product p on c.cid = p.cid inner join product_image pi on p.pid = pi.pid", nativeQuery = true)
+    List<IProduct> getProductsAllBy();
 }
